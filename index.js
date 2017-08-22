68345cab4a6c2f5eb006c233df0373fc47360021
viet-finda <viet@finda.co.kr>
2017-08-22 15:42:27 +0900

    update index location

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var BMModel_1 = require("./models/BMModel");
+exports.BMModel = BMModel_1.BMModel;
+var BMStatus_1 = require("./models/BMStatus");
+exports.BMStatus = BMStatus_1.BMStatus;
+var BMRole_1 = require("./models/BMRole");
+exports.BMRole = BMRole_1.BMRole;
+var BMGender_1 = require("./models/BMGender");
+exports.BMGender = BMGender_1.BMGender;
+var BDbLimitation_1 = require("./models/BDbLimitation");
+exports.BDbLimitation = BDbLimitation_1.BDbLimitation;
