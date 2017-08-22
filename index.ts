823d2b3dbc990a0d4d016bf921044db59e21874c
viet-finda <viet@finda.co.kr>
2017-08-22 14:06:19 +0900

    update index location

---
+++ index.ts
@@ -0,0 +1,39 @@
+import {IBMBook} from './models/interfaces/IBMBook';
+import {IBMBookText} from './models/interfaces/IBMBookText';
+import {IBMBookTextChapter} from './models/interfaces/IBMBookTextChapter';
+import {IBMConfigs} from './models/interfaces/IBMConfigs';
+import {IBMDbLimitation} from './models/interfaces/IBMDbLimitation';
+import {IBMEventModel} from './models/interfaces/IBMEventModel';
+import {IBMInfo} from './models/interfaces/IBMInfo';
+import {IBMPerson} from './models/interfaces/IBMPerson';
+import {IBMQuote} from './models/interfaces/IBMQuote';
+import {IBMSlider} from './models/interfaces/IBMSlider';
+import {IBMUser} from './models/interfaces/IBMUser';
+import {IBMUserBasicInfo} from './models/interfaces/IBMUserBasicInfo';
+import {IBMUserFriendship} from './models/interfaces/IBMUserFriendship';
+import {BMModel} from './models/BMModel';
+import {BMStatus} from './models/BMStatus';
+import {BMRole} from './models/BMRole';
+import {BMGender} from './models/BMGender';
+import {BDbLimitation} from './models/BDbLimitation';
+
+export {
+  IBMBook,
+  IBMBookText,
+  IBMBookTextChapter,
+  IBMConfigs,
+  IBMDbLimitation,
+  IBMEventModel,
+  IBMInfo,
+  IBMPerson,
+  IBMQuote,
+  IBMSlider,
+  IBMUser,
+  IBMUserBasicInfo,
+  IBMUserFriendship,
+  BMModel,
+  BMStatus,
+  BMRole,
+  BMGender,
+  BDbLimitation
+};
