37be2fdaf9c930c2fe09647d8e1e75b95d092313
viet-finda <viet@finda.co.kr>
2017-08-27 19:32:09 +0900

    Update BMBookType js files

---
+++ models/BMBookType.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BMBookType = {
+    text: {
+        code: 'text',
+        label: 'Text',
+        description: ''
+    },
+};
+//# sourceMappingURL=BMBookType.js.map
