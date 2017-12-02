2bbaefc7549c8e455c0f8300cd5e270971358111
vieth <viet.hoang.le88@gmail.com>
2017-12-02 18:02:20 +0900

    Added TBMOpenID.ts

---
+++ models/types/TBMOpenID.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var TBMOpenID;
+(function (TBMOpenID) {
+    var ProviderFB = 'facebook';
+    var ProviderGG = 'google';
+})(TBMOpenID || (TBMOpenID = {}));
+exports.default = TBMOpenID;
+//# sourceMappingURL=TBMOpenID.js.map
