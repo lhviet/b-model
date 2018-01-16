7b9181b74e782592ea2c7d60cb8370e605309bac
vieth <viet.hoang.le88@gmail.com>
2018-01-16 20:21:05 +0700

    update index

--- index.ts
+++ index.ts
@@ -15,12 +15,13 @@ export * from './models/interfaces/IBMUserActivity';
 
 export * from './models/interface_server_response/ISUser';
 
-export * from './models/BDbPostgres';
 export * from './models/BMModel';
 export * from './models/BMStatus';
 export * from './models/BMRole';
 export * from './models/BMGender';
 export * from './models/BMBookType';
+export * from './models/BMActivity';
+export * from './models/BDbPostgres';
 export * from './models/BDbLimitation';
 
 export * from './query/BMQuery';
